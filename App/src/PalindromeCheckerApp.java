896e41fb14d5e430c104a63f0175aafa2b5e8a43
saiteja051006 <mksaiteja705@gmail.com>
2026-02-20 10:54:13 +0530

    uc-1 program added

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+public class gitPalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("   Welcome to Palindrome Checker App   ");
+
+        System.out.println("Application Name : PalindromeChecker");
+        System.out.println("Version          : 1.0");
+
+        System.out.println("System initialized successfully.");
+
+    }
+}
